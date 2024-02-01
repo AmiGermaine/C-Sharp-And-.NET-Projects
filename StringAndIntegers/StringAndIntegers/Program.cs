0e10b28ddd08ff2c3ae397d15d7f95c603477ed5
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-01 15:01:00 -0800

    update

--- StringAndIntegers/StringAndIntegers/Program.cs
+++ StringAndIntegers/StringAndIntegers/Program.cs
@@ -10,36 +10,52 @@ namespace StringAndIntegers
     {
         static void Main(string[] args)
         {
-            
-            try //try/catch expection to catch errors that'll occur during execution 
+            List<int> numbers = new List<int>() { 2, 4, 6, 8, 10 };
+            foreach (int i in numbers)
             {
-                List<int> numbers = new List<int>() { 2, 4, 6, 8, 10 };
-                Console.WriteLine("Enter a number:");
-                int numOne = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine("Enter a second number.");
-                //int numTwo = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine("Dividing the two...");
-                //int numThree = numbers / numOne;
-                Console.WriteLine("result is: " + numThree );
-                
+                Console.WriteLine(i);
             }
-            catch (FormatException ex) //statement that will handle an expection when it occur
-            { 
-                Console.WriteLine("Please Enter numbers ONLY!"); 
-            }
-            catch (DivideByZeroException ex)
+            bool numVal = false;
+            while (!numVal) 
             {
-                Console.WriteLine("Can't divide by zero");
-            }
+                try //try/catch expection to catch errors that'll occur during execution 
+                {
+                    Console.WriteLine("Enter a number:");
+                    int numOne = Convert.ToInt32(Console.ReadLine());
+                    if (numOne == 0)
+                    {
+                        Console.WriteLine("Can't enter zero");
+                    }
+                    else
+                    {
+                        foreach (int i in numbers)
+                        {
+                            int divideresult = i / numOne;
+                            Console.WriteLine(i + " divide " + numOne + " equal " + divideresult);
+                        }
+                        numVal = true;
+                    }
 
-            catch (Exception ex)  //Catch exception to catch everything
-            {
-                Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex) //statement that will handle an expection when it occur
+                {
+                    Console.WriteLine("Please Enter numbers ONLY!");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Can't divide by zero");
+                }
+
+                catch (Exception ex)  //Catch exception to catch everything
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally //finally block to excute the code regardless if an exception is caught or not
+                {
+                    Console.ReadLine();
+                }
             }
-            finally //finally block to excute the code regardless if an exception is caught or not
-            {
-                Console.ReadLine(); 
-            }
+           
         }
     }
 }
