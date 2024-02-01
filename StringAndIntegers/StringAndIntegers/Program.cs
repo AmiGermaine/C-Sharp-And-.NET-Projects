fe0ee707d6e8cac25c5eb40f79bd4cfc5874562f
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-01 05:35:06 -0800

    update

---
+++ StringAndIntegers/StringAndIntegers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAndIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try //try/catch expection to catch errors that'll occur during execution 
+            {
+                Console.WriteLine("Enter a number:");
+                int numOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a second number.");
+                int numTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numThree = numOne / numTwo;
+                Console.WriteLine("result is: " + numThree );
+                
+            }
+            catch (FormatException ex) //statement that will handle an expection when it occur
+            { 
+                Console.WriteLine("Please Enter numbers ONLY!"); 
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Can't divide by zero");
+            }
+
+            catch (Exception ex)  //Catch exception to catch everything
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally //finally block to excute the code regardless if an exception is caught or not
+            {
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
