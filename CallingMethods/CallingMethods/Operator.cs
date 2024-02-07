996f4d48329295796672f64256a4b289d08402b8
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-07 11:23:28 -0800

    update

---
+++ CallingMethods/CallingMethods/Operator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public static class Operator //creating a class call operator with three methods that will do some math operation.
+    {
+        public static int Add(int x)  //First method to be call 
+        {
+            return x + 4;  //returns data back when the Add method is invoked
+        }
+
+        public static int Subtract(int x)  //Secong method to be call
+        {
+            return  x - 3;
+        }
+
+        public static int Divide(int x) //last method to be call
+        {
+            return x / 2;
+        }
+        //public int data {  get; set; }
+        
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using CallingMethods;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:"); //user input
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int result = Operator.Add(x); 
+            Console.WriteLine(result);
+
+            int resulttwo = Operator.Subtract(x);
+            Console.WriteLine(resulttwo); //ths will print the result of the subtraction operator
+
+            int result3 = Operator.Divide(x);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();        
+        }
+    }
+}
