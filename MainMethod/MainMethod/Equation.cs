cd6b31a008767bcb7eb7d165c94ed499910d0423
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-07 17:50:43 -0800

    added

---
+++ MainMethod/MainMethod/Equation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+     public class Equation //
+    {
+        public int Multiply(int y)  //first method that take in an integer
+        {
+            return y * 5;
+        }
+
+        public decimal Multiply(decimal z) // second method that take in  a decimal
+        {
+            return z * 6;
+        }
+
+        public int Multiply(string id) //third method using  string that get to convert to an integer
+        {
+            int num = Convert.ToInt32(id);
+            return num * 7;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:"); //user input
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Equation equation = new Equation();  //Instantiate
+            Console.WriteLine(equation.Multiply(y));
+
+            Console.WriteLine(equation.Multiply(7.3m)); //pasing decimal 
+
+            Console.WriteLine(equation.Multiply("4")); //passing in a string that equates to an integer
+
+
+            Console.ReadLine();
+        }
+    }
+}
