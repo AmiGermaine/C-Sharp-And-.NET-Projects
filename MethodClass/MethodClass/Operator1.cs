f7c57e2782cf9e1f65214a5d432a503bdf188f0c
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-12 08:43:27 -0800

    update

---
+++ MethodClass/MethodClass/Operator1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Operator1
+    {
+        public void sum(int numberOne, int numberTwo) // creating a void method that takes two integers as parameters.
+        {
+            Console.WriteLine("sub = " + (numberOne - numberTwo));
+        }
+        public void sum1(int numberTwo)
+        {
+            Console.WriteLine(numberTwo);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calling function
+            Operator1 op1 = new Operator1();  //Instantiate
+            op1.sum(50, 20); //math operation
+            op1.sum1(numberTwo:10);
+
+            Console.ReadLine();
+        }
+    }
+}
