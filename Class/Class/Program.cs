02c7678c82a53e3f9615963e1f83a2c84288c274
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-12 10:20:30 -0800

    update

---
+++ Class/Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            TestClass testClass = new TestClass();  //Instantiate
+
+            Console.WriteLine("Please enter a number: "); //user input
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            testClass.Math(number, out int result); //the method that get call
+
+            Console.WriteLine(result); //output
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class/Class/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class TestClass
+    {
+        public void Math(int number, out int result)
+        {
+            result = number / 2;
+        }
+        public void Math(int number, int dividebytwo, out int result) //Overload method.
+        {
+            result = number / dividebytwo;
+            
+        }
+
+    }
+}
