8b163754645105cfc8803e486a654778f10f7872
Amida <143059696+AmiGermaine@users.noreply.github.com>
2024-02-26 03:27:23 -0800

    update

---
+++ SunSchool/SunSchool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SunSchool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
